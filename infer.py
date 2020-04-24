6c8d4c7f38dd321c16d3f51974a57ec7a983ee82
yeyupiaoling <yeyupiaoling@foxmail.com>
2020-04-24 11:44:32 +0800

    first commit

---
+++ infer.py
@@ -0,0 +1,41 @@
+import librosa
+import numpy as np
+import paddle.fluid as fluid
+
+# 创建执行器
+place = fluid.CPUPlace()
+exe = fluid.Executor(place)
+exe.run(fluid.default_startup_program())
+
+# 保存预测模型路径
+save_path = 'models/'
+# 从模型中获取预测程序、输入数据名称列表、分类器
+[infer_program,
+ feeded_var_names,
+ target_var] = fluid.io.load_inference_model(dirname=save_path, executor=exe)
+
+
+# 读取音频数据
+def load_data(data_path):
+    y1, sr1 = librosa.load(data_path, duration=2.97)
+    ps = librosa.feature.melspectrogram(y=y1, sr=sr1).astype(np.float32)
+    ps = ps[np.newaxis, np.newaxis, ...]
+    return ps
+
+
+def infer(audio_path):
+    data = load_data(audio_path)
+    # 执行预测
+    result = exe.run(program=infer_program,
+                     feed={feeded_var_names[0]: data},
+                     fetch_list=target_var)
+    # 显示图片并输出结果最大的label
+    lab = np.argsort(result)[0][0][-1]
+    return lab
+
+
+if __name__ == '__main__':
+    # 要预测的音频文件
+    path = 'dataset/audio/peibin/ba6d1294-e330-11e9-a517-c8ff285a4318.wav'
+    label = infer(path)
+    print('音频：%s 的预测结果标签为：%d' % (path, label))
---
+++ train.py
@@ -0,0 +1,93 @@
+import os
+
+import reader
+import paddle.fluid as fluid
+
+# 保存预测模型路径
+save_path = 'models/'
+# 类别总数
+CLASS_DIM = 5
+
+# 定义输入层
+image = fluid.data(name='image', shape=[None, 1, 128, 128], dtype='float32')
+label = fluid.data(name='label', shape=[None, 1], dtype='int64')
+
+
+# 卷积神经网络
+def cnn(input, class_dim):
+    conv1 = fluid.layers.conv2d(input=input,
+                                num_filters=20,
+                                filter_size=5,
+                                act='relu')
+
+    conv2 = fluid.layers.conv2d(input=conv1,
+                                num_filters=50,
+                                filter_size=5,
+                                act='relu')
+
+    pool1 = fluid.layers.pool2d(input=conv2,
+                                pool_size=2,
+                                pool_stride=2,
+                                pool_type='max')
+
+    bn = fluid.layers.batch_norm(pool1)
+    flatten = fluid.layers.flatten(bn)
+    f1 = fluid.layers.fc(input=flatten, size=128, act='relu')
+    f2 = fluid.layers.fc(input=f1, size=class_dim, act='softmax')
+    return f2
+
+
+# 获取网络模型
+model = cnn(image, CLASS_DIM)
+
+# 获取损失函数和准确率函数
+cost = fluid.layers.cross_entropy(input=model, label=label)
+avg_cost = fluid.layers.mean(cost)
+acc = fluid.layers.accuracy(input=model, label=label)
+
+# 获取训练和测试程序
+test_program = fluid.default_main_program().clone(for_test=True)
+
+# 定义优化方法
+optimizer = fluid.optimizer.AdamOptimizer(learning_rate=1e-3)
+opts = optimizer.minimize(avg_cost)
+
+# 获取自定义数据
+train_reader = reader.train_reader('dataset/train', batch_size=32)
+test_reader = reader.test_reader('dataset/test', batch_size=32)
+
+# 定义一个使用GPU的执行器
+place = fluid.CUDAPlace(0)
+exe = fluid.Executor(place)
+# 进行参数初始化
+exe.run(fluid.default_startup_program())
+
+# 训练
+for pass_id in range(100):
+    # 进行训练
+    for batch_id, data in enumerate(train_reader()):
+        train_cost, train_acc = exe.run(program=fluid.default_main_program(),
+                                        feed={image.name: data[0], label.name: data[1]},
+                                        fetch_list=[avg_cost, acc])
+
+        # 每100个batch打印一次信息
+        if batch_id % 100 == 0:
+            print('Pass:%d, Batch:%d, Cost:%0.5f, Accuracy:%0.5f' %
+                  (pass_id, batch_id, train_cost[0], train_acc[0]))
+
+    # 进行测试
+    test_accs = []
+    test_costs = []
+    for batch_id, data in enumerate(test_reader()):
+        test_cost, test_acc = exe.run(program=test_program,
+                                      feed={image.name: data[0], label.name: data[1]},
+                                      fetch_list=[avg_cost, acc])
+        test_accs.append(test_acc[0])
+        test_costs.append(test_cost[0])
+    # 求测试结果的平均值
+    test_cost = (sum(test_costs) / len(test_costs))
+    test_acc = (sum(test_accs) / len(test_accs))
+    print('Test:%d, Cost:%0.5f, Accuracy:%0.5f' % (pass_id, test_cost, test_acc))
+
+    # 保存预测模型
+    fluid.io.save_inference_model(dirname=save_path, feeded_var_names=[image.name], target_vars=[model], executor=exe)
